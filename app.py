4e4bc04af146805ccdf1dad47c7fd7467948e1fc
Mammadli <il51ycyn@fauad.fau.de>
2019-09-24 15:15:26 +0200

    some minor fixes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 port = int(os.environ["PORT"])
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
     return 'Home Page'
 
