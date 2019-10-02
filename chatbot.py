e790ff4737abf9be85efff838fb19d4e747b38d8
fauchatbot <55593023+fauchatbot@users.noreply.github.com>
2019-10-02 13:55:38 +0200

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -147,7 +147,7 @@ def mensa():
     txt = re.sub('[\n\r\xa0]', '', final_text)
     txt = re.sub(' +', ' ',txt)
     txt = re.split('Essen [1-9]', txt)
-    essen_list = [{"type": "list", "content": {"elements":[]},"delay": 'null'}]
+    essen_list = [{"type": "list", "content": {"elements":[]}}]
     intermediate_list = []
 
     for i in enumerate(txt):
